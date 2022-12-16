a0e756eaea31f22fa6587d405013a2f76222ab31
mikka <kaelaquiton@gmail.com>
2022-12-16 16:21:39 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,8 +1,10 @@
 package SimpleCalc;
 
-import LeapYear.LeapYearGUI;
-
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel panel1;
@@ -10,14 +12,67 @@ public class SimpleCalcGUI extends JFrame{
     private JComboBox cbOperations;
     private JButton btnCompute;
     private JTextField tfNumber2;
-    private JLabel lblResult;
+    private JTextField lblResult;
 
-    public SimpleCalcGUI(String title){
-        super(title);
+
+    public SimpleCalcGUI(){
+
+    btnCompute.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Calc();
+        }
+    });
+
+        lblResult.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    Calc();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+
     }
 
-    public static void main(String[] args){
+    public void Calc(){
 
+            int num1 = Integer.parseInt(tfNumber1.getText());
+            int num2 = Integer.parseInt(tfNumber2.getText());
+            int result = 0;
+            switch (cbOperations.getSelectedItem().toString()) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+            lblResult.setText(String.valueOf(result));
     }
 
+    public static void main(String[] args) {
+        SimpleCalcGUI app = new SimpleCalcGUI();
+        app.setTitle("Simple Calculator");
+        app.setVisible(true);
+        app.setSize(700, 500);
+        app.setContentPane(app.panel1);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }
